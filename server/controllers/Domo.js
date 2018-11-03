8b58076018424ac903d0361103cb56074c5c352e
Jarrett Briody <jarrettbriody@gmail.com>
2018-11-03 17:08:51 -0400

    finished

---
+++ server/controllers/Domo.js
@@ -0,0 +1,76 @@
+const models = require('../models');
+
+const Domo = models.Domo;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
+  });
+};
+
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.iq) {
+    return res.status(400).json({ error: 'RAWR! Name, age and IQ are all required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    iq: req.body.iq,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists.' });
+    }
+
+    return res.status(400).json({ error: 'An error occurred' });
+  });
+
+  return domoPromise;
+};
+
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.json({ domos: docs });
+  });
+};
+
+const deleteDomo = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.deleteOneDomo(req.session.account._id, req.body.name, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.status(200).json({ redirect: '/maker' });
+  });
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
+module.exports.deleteDomo = deleteDomo;
+module.exports.getDomos = getDomos;
